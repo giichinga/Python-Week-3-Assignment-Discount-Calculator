bfd40ed18bf411e2a2b741dfd50b38ccf896e858
Tonny Njoroge <antonnygichinga97@gmail.com>
2025-08-14 10:52:12 +0300

    updated discount.py

--- discount.py
+++ discount.py
@@ -1,1 +1,12 @@
+print("What is the price of the product?")
+price = float(input())
+print("What is the discount percentage?")
+discountPercent = float(input())
 
+def calculateDiscount(price, discountPercent):
+    discountAmount = price * (discountPercent / 100)
+    return discountAmount
+
+discountAmount = calculateDiscount(price, discountPercent)
+print(f"Discount Amount: Ksh.{discountAmount:.2f}")
+print(f"Final Price after Discount: Ksh.{price - discountAmount:.2f}")
